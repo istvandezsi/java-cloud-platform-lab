823da1eb6bf61ec5473aac570737ba66f462fcdd
István András Dézsi <dezsi.istvan84@gmail.com>
2026-07-26 16:56:12 +0200

    Align OpenAPI version with project release

--- src/main/java/hu/dezsi/cloudlab/OpenApiConfig.java
+++ src/main/java/hu/dezsi/cloudlab/OpenApiConfig.java
@@ -2,6 +2,7 @@ package hu.dezsi.cloudlab;
 
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import org.springframework.boot.info.BuildProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -9,11 +10,11 @@ import org.springframework.context.annotation.Configuration;
 public class OpenApiConfig {
 
     @Bean
-    OpenAPI openApi() {
+    OpenAPI openApi(BuildProperties buildProperties) {
         return new OpenAPI()
                 .info(new Info()
                         .title("Java Cloud Platform Lab API")
                         .description("API documentation for the Java Cloud Platform Lab task service.")
-                        .version("0.0.1"));
+                        .version(buildProperties.getVersion()));
     }
 }
